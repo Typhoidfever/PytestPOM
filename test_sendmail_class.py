dcf880774c5c1b8b983fbf97a254f43e17d8c7da
Typhoid <kgalkin1985@gmail.com>
2023-03-13 21:00:25 +0200

    Python Page Object Model

--- test_sendmail_class.py
+++ test_sendmail_class.py
@@ -7,11 +7,11 @@ class TestSendEmailTest:
         self.tempmail_site = EmailSendHelper(browser)
         self.tempmail_site.go_to_site()
 
-        for i in range(5):
+        for i in range(15):
             self.tempmail_site.email_address_copy()
             self.tempmail_site.create_and_send_new_test_email()
             i += 1
         self.tempmail_site.create_and_send_summary_test_email()
-        for j in range(5):
+        for j in range(15):
             self.tempmail_site.delete_test_emails()
             j += 1
