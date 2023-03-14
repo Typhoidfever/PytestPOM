f2af2fb4b09dcc8025ba7bae16cb6bf4c077bf52
Typhoid <kgalkin1985@gmail.com>
2023-03-14 09:23:15 +0200

    Python Page Object Model

--- conftest.py
+++ conftest.py
@@ -1,12 +1,14 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="session")
 def browser():
-    driver: WebDriver = webdriver.Chrome(
-        executable_path="C://Users//user//PycharmProjects//PytestPOM//chromedriver.exe")
+    chrome_options = Options()
+    chrome_options.add_extension('C:\\Users\\user\\PycharmProjects\\PytestPOM\\extension_3_16_2_0.crx')
+    driver: WebDriver = webdriver.Chrome(chrome_options=chrome_options)
     driver.maximize_window()
     yield driver
     driver.quit()
